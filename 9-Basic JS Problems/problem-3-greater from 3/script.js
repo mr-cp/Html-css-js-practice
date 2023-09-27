1a7c4526a2ad541f90e527d8793b53b47d5cacf6
mr-cp <rishad551@gmail.com>
2023-09-27 22:57:39 +0530

    Basic JavaScript problem for practicing

---
+++ 9-Basic JS Problems/problem-3-greater from 3/script.js
@@ -0,0 +1,13 @@
+let a = prompt("Enter 1st number")
+let b = prompt("Enter 2nd number")
+let c = prompt("Enter 3rd number")
+
+document.write("Entered numbers are: " + a + ", " + b + ", " + c+'.')
+
+if (a > b && a > c) {
+    document.write('<br>' + a + ' is greater.')
+} else if (b > c) {
+    document.write('<br>' + b + ' is greater.')
+} else {
+    document.write('<br>' + c + ' is greater.')
+}
